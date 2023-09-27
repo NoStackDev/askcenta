17011bf9d7dee287dcac716b74d7eee420e901fa
NoStackDev <partialstackdev@gmail.com>
2023-09-27 02:40:03 +0100

    update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,14 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "",
         nav: "font-poppins font-medium text-base text-[#6356E5]",
+        request:
+          "font-roboto font-medium text-base text-white py-[6px] px-6 bg-primary rounded-[20px]",
         request_outlined:
           "font-roboto font-normal text-xs text-[#6356E5] py-[6px] px-4 rounded-xl border border-[#E5E4F0] hover:bg-[#6356E5] hover:text-white",
       },
