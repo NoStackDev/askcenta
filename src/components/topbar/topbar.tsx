b052de5dc9f81dc7a141ce164a66c73051546afe
NoStackDev <partialstackdev@gmail.com>
2023-10-06 15:17:12 +0100

    fix: categories sub select now works

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -5,59 +5,75 @@ import { KeyboardBackspaceIcon, LocationIcon, PersonIcon } from "../icons";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import TopbarSelect from "./topbar_select";
+import { SubCategoryResponseType } from "../../../types";
+
+async function fetchSubCategories() {
+  const res = await fetch("https://www.askcenta.ng/api/categoryGroups", {
+    method: "OPTIONS",
+    next: {
+      revalidate: 3600 * 6,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch feed");
+  }
+
+  return res.json();
+}
 
 interface TopbarProps extends React.HTMLAttributes<HTMLDivElement> {
   subcategoryid: string | string[] | undefined;
 }
-const Topbar = React.forwardRef<React.ElementRef<typeof Card>, TopbarProps>(
-  ({ className, subcategoryid, ...props }, forwardRef) => {
-    return (
-      <>
-        {!subcategoryid && (
-          <Card
-            ref={forwardRef}
-            className={cn("", className)}
-            {...props}
-            variant="card1"
-          >
-            <CardContent className="flex flex-col gap-5">
-              <div className="w-full bg-[#F2F4F8] h-12 rounded-3xl flex items-center px-3 gap-2">
-                <PersonIcon className="" />
+export default async function Topbar({
+  className,
+  subcategoryid,
+  ...props
+}: TopbarProps) {
+  const subCategoriesRes: Promise<SubCategoryResponseType> =
+    fetchSubCategories();
+  const subCategories = await subCategoriesRes;
 
-                <div className="font-roboto font-medium text-base opacity-60 text-black">
-                  What do you need?
-                </div>
+  return (
+    <>
+      {!subcategoryid && (
+        <Card className={cn("", className)} {...props} variant="card1">
+          <CardContent className="flex flex-col gap-5">
+            <div className="w-full bg-[#F2F4F8] h-12 rounded-3xl flex items-center px-3 gap-2">
+              <PersonIcon className="" />
+
+              <div className="font-roboto font-medium text-base opacity-60 text-black">
+                What do you need?
+              </div>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <LocationIcon className="mx-2" aria-label="location" />
+
+                <span className="font-roboto font-normal text-xs text-black opacity-60">
+                  Nationwide
+                </span>
               </div>
 
-              <div className="flex justify-between items-center">
-                <div className="flex items-center">
-                  <LocationIcon className="mx-2" aria-label="location" />
+              <Button variant="request_outlined">Post request</Button>
+            </div>
+          </CardContent>
+        </Card>
+      )}
 
-                  <span className="font-roboto font-normal text-xs text-black opacity-60">
-                    Nationwide
-                  </span>
-                </div>
+      {subcategoryid && (
+        <Card variant="card1">
+          <Link href="/" className="flex w-fit h-fit">
+            <KeyboardBackspaceIcon aria-label="back" />
+          </Link>
 
-                <Button variant="request_outlined">Post request</Button>
-              </div>
-            </CardContent>
-          </Card>
-        )}
-
-        {subcategoryid && (
-          <Card variant="card1">
-            <Link href="/" className="flex w-fit h-fit">
-              <KeyboardBackspaceIcon aria-label="back" />
-            </Link>
-
-            <TopbarSelect subcategoryid={subcategoryid}></TopbarSelect>
-          </Card>
-        )}
-      </>
-    );
-  }
-);
-
-Topbar.displayName = "Topbar";
-
-export default Topbar;
+          <TopbarSelect
+            subcategoryid={subcategoryid}
+            subcategories={subCategories.data}
+          ></TopbarSelect>
+        </Card>
+      )}
+    </>
+  );
+}
--- src/components/topbar/topbar_select.tsx
+++ src/components/topbar/topbar_select.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Select,
@@ -9,41 +11,27 @@ import {
   SelectTriggerIcon,
   SelectValue,
 } from "../ui/select";
-import { SubCategoryResponseType } from "../../../types";
+import { SubCategoryType } from "../../../types";
 import { ChevronRightIcon } from "../icons";
-import TopbarSelectLink from "./topbar_select_link";
-
-async function fetchSubCategories() {
-  const res = await fetch("https://www.askcenta.ng/api/categoryGroups", {
-    method: "OPTIONS",
-    next: {
-      revalidate: 3600 * 6,
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error("failed to fetch feed");
-  }
-
-  return res.json();
-}
+import { useRouter } from "next/navigation";
 
 interface TopbarSelectProps extends React.HTMLAttributes<typeof Select> {
   subcategoryid: string | string[] | undefined;
+  subcategories: SubCategoryType[];
 }
 
-export default async function TopbarSelect({
+export default function TopbarSelect({
   className,
   subcategoryid,
+  subcategories,
   ...props
 }: TopbarSelectProps) {
-  const subCategoriesRes: Promise<SubCategoryResponseType> =
-    fetchSubCategories();
-  const subCategories = await subCategoriesRes;
-  const subCategoryData = subCategories.data.find(
+  const router = useRouter();
+
+  const subCategoryData = subcategories.find(
     (subCategory) => subCategory.id === Number(subcategoryid as string)
   );
-  const category = subCategories.data.filter(
+  const category = subcategories.filter(
     (subCategory) => subCategory.category === subCategoryData?.category
   );
 
@@ -53,7 +41,9 @@ export default async function TopbarSelect({
         {subCategoryData?.category.trim()}
       </div>
 
-      <Select>
+      <Select
+        onValueChange={(value) => router.push(`/?category_group_id=${value}`)}
+      >
         <SelectTrigger className="group border border-[#D9D9D9] rounded-lg p-3 flex items-center gap-5 font-roboto font-medium text-sm text-[#5E5D7F] min-w-[100px]">
           <SelectValue placeholder={subCategoryData?.name.trim()} />
           <SelectTriggerIcon>
@@ -65,10 +55,12 @@ export default async function TopbarSelect({
           <SelectGroup className="flex flex-col items-start gap-2">
             {category.map((subCategory) => {
               return (
-                <TopbarSelectLink
-                  subcategory={subCategory}
+                <SelectItem
+                  value={subCategory.id.toString()}
                   key={subCategory.id}
-                />
+                >
+                  <SelectItemText>{subCategory.name.trim()}</SelectItemText>
+                </SelectItem>
               );
             })}
           </SelectGroup>
