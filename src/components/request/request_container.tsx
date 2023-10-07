3f846edf61d3e869a88a5f6ac8d7d1ee79938646
NoStackDev <partialstackdev@gmail.com>
2023-10-07 23:03:53 +0100

    update

--- src/components/request/request_container.tsx
+++ src/components/request/request_container.tsx
@@ -15,8 +15,6 @@ async function fetchFeed(searchParams?: {
       ""
     );
 
-    // console.log(`https://www.askcenta.ng/api/feeds?${params.slice(0, params.length - 1)}`);
-
     const res = await fetch(
       `https://www.askcenta.ng/api/feeds?${params.slice(0, params.length - 1)}`,
       {
