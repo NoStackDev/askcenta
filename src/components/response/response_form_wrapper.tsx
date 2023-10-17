bbb05583e7dfd19dbc75f0a09f3b94c5d474b421
NoStackDev <partialstackdev@gmail.com>
2023-10-17 20:36:29 +0100

    update

--- src/components/response/response_form_wrapper.tsx
+++ src/components/response/response_form_wrapper.tsx
@@ -1,62 +1,30 @@
 import React from "react";
 import { CitiesResponseType, StateResponseType } from "../../../types";
 import ResponseForm from "./response_form";
-// import { fetchCities, fetchStates } from "@/helpers/location";
-
-async function fetchCities() {
-  const res = await fetch("https://www.askcenta.ng/api/cities", {
-    method: "OPTIONS",
-    next: {
-      revalidate: 3600 * 6,
-    },
-  });
-
-  if (!res.ok)
-    throw new Error(
-      "failed to fetch cities from https://www.askcenta.ng/api/cities"
-    );
-
-  return res.json();
-}
-
-async function fetchStates() {
-  const res = await fetch("https://askcenta.ng/api/states", {
-    method: "OPTIONS",
-    next: {
-      revalidate: 3600 * 6,
-    },
-  });
-
-  if (!res.ok)
-    throw new Error(
-      "failed to fetch states from https://askcenta.ng/api/states"
-    );
-
-  return res.json();
-}
+import { fetchCities, fetchStates } from "@/api/location";
+import { DialogProps } from "@radix-ui/react-dialog";
 
 interface ResponseFormWrapperProps
-  extends React.HTMLAttributes<typeof ResponseForm> {
+  extends React.HTMLAttributes<DialogProps> {
   params: { id: string };
 }
 
-export default function ResponseFormWrapper({
+export default async function ResponseFormWrapper({
   className,
   children,
   params,
 }: ResponseFormWrapperProps) {
-  // const citiesRes: Promise<CitiesResponseType> = fetchCities();
-  // const statesRes: Promise<StateResponseType> = fetchStates();
+  const citiesRes: Promise<CitiesResponseType> = fetchCities();
+  const statesRes: Promise<StateResponseType> = fetchStates();
 
-  // const [cities, states] = await Promise.all([citiesRes, statesRes]);
+  const [cities, states] = await Promise.all([citiesRes, statesRes]);
   return (
-    // <ResponseForm
-    //   params={params}
-    //   citiesdata={cities.data}
-    //   statesdata={states.data}
-    // >
-    //   {children}
-    // </ResponseForm>
-    <div>lllll</div>
+    <ResponseForm
+      params={params}
+      citiesdata={cities.data}
+      statesdata={states.data}
+    >
+      {children}
+    </ResponseForm>
   );
 }
