bfb80eed33d41ba74e78a2be961ce4878efadd81
NoStackDev <partialstackdev@gmail.com>
2023-10-17 19:26:40 +0100

    update

--- src/app/nearby/nearby_location.tsx
+++ src/app/nearby/nearby_location.tsx
@@ -4,7 +4,39 @@ import { CitiesResponseType, StateResponseType } from "../../../types";
 import NearbyLocationForm from "./nearby_location_form";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { fetchCities, fetchStates } from "@/helpers/location";
+// import { fetchCities, fetchStates } from "@/helpers/location";
+
+async function fetchCities() {
+  const res = await fetch("https://www.askcenta.ng/api/cities", {
+    method: "OPTIONS",
+    next: {
+      revalidate: 3600 * 6,
+    },
+  });
+
+  if (!res.ok)
+    throw new Error(
+      "failed to fetch cities from https://www.askcenta.ng/api/cities"
+    );
+
+  return res.json();
+}
+
+async function fetchStates() {
+  const res = await fetch("https://askcenta.ng/api/states", {
+    method: "OPTIONS",
+    next: {
+      revalidate: 3600 * 6,
+    },
+  });
+
+  if (!res.ok)
+    throw new Error(
+      "failed to fetch states from https://askcenta.ng/api/states"
+    );
+
+  return res.json();
+}
 
 interface NearbyLocationProps extends React.HTMLAttributes<HTMLDivElement> {
   cityid: string | string[] | undefined;
