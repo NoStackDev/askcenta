325bb333435f2f0f10306097b462f4b292aeb0a9
NoStackDev <partialstackdev@gmail.com>
2023-11-13 23:55:56 +0100

    removed dynamic id route, used search params instead

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import ProfileTopbar from "./profile_topbar";
 import ProfileTabs from "./profile_tabs";
 
-type Props = {};
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default function ProfilePage({}: Props) {
+export default function ProfilePage({ searchParams }: Props) {
+  const otherUserId = searchParams.user;
   return (
     <main className="w-full">
-      <ProfileTopbar />
+      {otherUserId ? <ProfileTopbar otheruser /> : <ProfileTopbar />}
 
-      <ProfileTabs className="mt-4" />
+      {otherUserId ? (
+        <ProfileTabs otheruser className="mt-4" />
+      ) : (
+        <ProfileTabs className="mt-4" />
+      )}
     </main>
   );
 }
