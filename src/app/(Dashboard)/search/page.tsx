1d23c8b6bf27ceda5e144b3b64f7bb3d4a90a78c
NoStackDev <partialstackdev@gmail.com>
2023-12-26 01:15:47 +0100

    minor fix

--- src/app/(Dashboard)/search/page.tsx
+++ src/app/(Dashboard)/search/page.tsx
@@ -2,9 +2,10 @@ import { RequestContainer } from "@/components/request";
 import React from "react";
 import Searchbar from "./search_bar";
 import SearchTopbar from "./search_topbar";
-import { getFeedsActions } from "@/actions";
-import { FeedsResponse } from "@/types";
+import { fetchBookmarksAction, getFeedsActions } from "@/actions";
+import { FeedsResponse, RequestType, UserDetailsType } from "@/types";
 import { SearchIllustration } from "@/components/icons";
+import { cookies } from "next/headers";
 
 type Props = {
   params: { slug: string };
@@ -12,9 +13,34 @@ type Props = {
 };
 
 export default async function SearchPage({ searchParams }: Props) {
+  const cookie = cookies();
+  const user: UserDetailsType["data"] | null = JSON.parse(
+    cookie.get("user")?.value || "null"
+  );
+
   const cityId = searchParams.city_id;
   const feedres: Promise<FeedsResponse> = getFeedsActions(searchParams);
   const requests = (await feedres).data;
+
+  let requestsWithBookmarks: RequestType[] = [...requests];
+
+  if (user) {
+    try {
+      const bookmarkedUserRequests: { data: RequestType[] } =
+        await fetchBookmarksAction();
+      requestsWithBookmarks = requests.map((request) => {
+        const matchedBookmark = bookmarkedUserRequests.data.find(
+          (bookmarkedRequest) => bookmarkedRequest.id === request.id
+        );
+        if (matchedBookmark) {
+          return matchedBookmark;
+        }
+        return request;
+      });
+    } catch (err) {
+      console.log(`failed trying to fetch bookmarks for user `, err);
+    }
+  }
 
   return (
     <main className="w-full">
@@ -23,9 +49,9 @@ export default async function SearchPage({ searchParams }: Props) {
       </div>
 
       <SearchTopbar cityid={cityId} className="mt-10" />
-      <RequestContainer requests={requests} />
+      <RequestContainer requests={requestsWithBookmarks} />
 
-      {requests.length === 0 && (
+      {requestsWithBookmarks.length === 0 && (
         <div className="w-full py-12 md:py-24 flex flex-col items-center justify-center">
           <SearchIllustration />
 
